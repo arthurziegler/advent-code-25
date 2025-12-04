cfa8b7d22b7eee32ab13473fb77703c1bf4dbda2
arthurziegler <arthurwziegler@gmail.com>
2025-12-03 21:28:57 -0300

    day 3 part 1 complete

--- day-3-lobby.py
+++ day-3-lobby.py
@@ -8,17 +8,24 @@ def main():
     total_output = 0
     # Find the highest battery
     for bank in battery_banks:
-        top_joltage = (0, 0)
+        top_joltage = [0, 0]
         for battery_idx in range(len(bank)):
             battery = int(bank[battery_idx])
-            if battery_idx < len(bank):
-                if battery > top_joltage[0]:
-                    top_joltage[0] = battery
-                    top_joltage[1] = 0
-            if battery <= top_joltage[0] and battery > top_joltage[1]:
+            if battery > top_joltage[0] and battery_idx < len(bank) - 1:
+                top_joltage[0] = battery
+                top_joltage[1] = 0
+            elif battery > top_joltage[1]:
                 top_joltage[1] = battery
-            print()
-        total_output += int(top_joltage[0] + top_joltage[1])
-            
+        print(bank)
+        print(top_joltage)
+        print(top_joltage[0]*10 + top_joltage[1])
+        total_output += top_joltage[0]*10 + top_joltage[1]
+    print(f"Total Output: {total_output}")
+    
+    
+    
 if __name__ == "__main__":
     main()
+    
+    
+    2525252122335344154213543554324533232121114451542423242331144254232355335331541343523141344524346789

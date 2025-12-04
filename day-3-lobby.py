fed6dbd19818eb48adc0386b88bd5c77e0cc7bdc
arthurziegler <arthurwziegler@gmail.com>
2025-12-04 01:44:04 -0300

    Day 3 task 2 complete

--- day-3-lobby.py
+++ day-3-lobby.py
@@ -4,18 +4,46 @@ def main():
         battery_banks = [line.strip() for line in file.readlines()]
     
     total_output = 0
+    BATTERY_SIZE = 12
+    
     # Find the highest battery
     for bank in battery_banks:
-        top_joltage = [0, 0]
-        for idx, battery in enumerate(bank):
-            battery = int(battery)
-            if battery > top_joltage[0] and idx < len(bank) - 1:
-                top_joltage[0] = battery
-                top_joltage[1] = 0
-            elif battery > top_joltage[1]:
-                top_joltage[1] = battery
-        #print(top_joltage[0]*10 + top_joltage[1])
-        total_output += top_joltage[0]*10 + top_joltage[1]
+        # Create a dictionary in the format: {on_idx: [bank_idx, joltage]}
+        online_battery = [[0,0] for _ in range(BATTERY_SIZE)]
+        prev_slot_idx = 0
+        bank_output = ''
+        
+        #Find each of the twelve batteries that need to be online
+        for slot_idx in range(BATTERY_SIZE):
+            #Battery to turn on will be chosen from the following sub-string of bank
+            slice_end = -(BATTERY_SIZE - slot_idx)+1
+            if slice_end == 0:
+                slice_end = None
+            valid_bank_win = bank[prev_slot_idx:slice_end]
+            
+            #Loop through the sub-string of bank
+            for win_idx, battery in enumerate(valid_bank_win, start=prev_slot_idx):
+                battery = int(battery)
+                if battery > online_battery[slot_idx][1]:
+                    online_battery[slot_idx][0] = win_idx
+                    online_battery[slot_idx][1] = battery
+                    prev_slot_idx = win_idx + 1
+                    
+            bank_output += str(online_battery[slot_idx][1])
+            
+        total_output += int(bank_output)
+
+        
+        
+        # # Part 1 solution
+        # for idx, battery in enumerate(bank):
+        #     battery = int(battery)           
+        #     if battery > top_joltage[0] and idx < len(bank) - 1:
+        #         top_joltage[0] = battery
+        #         top_joltage[1] = 0
+        #     elif battery > top_joltage[1]:
+        #         top_joltage[1] = battery
+  
     print(f"Total Output: {total_output}")
     
     

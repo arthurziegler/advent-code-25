8434de9f2fb873a6eea4443ce7cf828e673a56c9
arthurziegler <arthurwziegler@gmail.com>
2025-12-03 21:33:39 -0300

    improved day 3 part 1

--- day-3-lobby.py
+++ day-3-lobby.py
@@ -1,6 +1,4 @@
 def main():
-    test_input = "939800232324"
-
     #Read the input file
     with open("day-3-input.txt", "r") as file:
         battery_banks = [line.strip() for line in file.readlines()]
@@ -9,16 +7,14 @@ def main():
     # Find the highest battery
     for bank in battery_banks:
         top_joltage = [0, 0]
-        for battery_idx in range(len(bank)):
-            battery = int(bank[battery_idx])
-            if battery > top_joltage[0] and battery_idx < len(bank) - 1:
+        for idx, battery in enumerate(bank):
+            battery = int(battery)
+            if battery > top_joltage[0] and idx < len(bank) - 1:
                 top_joltage[0] = battery
                 top_joltage[1] = 0
             elif battery > top_joltage[1]:
                 top_joltage[1] = battery
-        print(bank)
-        print(top_joltage)
-        print(top_joltage[0]*10 + top_joltage[1])
+        #print(top_joltage[0]*10 + top_joltage[1])
         total_output += top_joltage[0]*10 + top_joltage[1]
     print(f"Total Output: {total_output}")
     
@@ -26,6 +22,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-    
-    
-    2525252122335344154213543554324533232121114451542423242331144254232355335331541343523141344524346789

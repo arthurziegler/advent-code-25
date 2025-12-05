884c60b7ab88b1a792938af245e8ea94b36644f6
arthurziegler <arthurwziegler@gmail.com>
2025-12-04 23:51:04 -0300

    Day 4 task 2 complete

--- day-4-printing-department.py
+++ day-4-printing-department.py
@@ -12,27 +12,33 @@ def main():
     #print(paper_matrix[0][9])
     
     available_paper_rolls = 0
-    for i, line in enumerate(paper_matrix):
-        for j, cell in enumerate(line):
-            paper_count = 0
-            print(f"Matrix Position: {cell} [{i},{j}]")
+    removable_rolls = True
+    while removable_rolls:
+        removable_rolls = False
+        for i, line in enumerate(paper_matrix):
+            for j, cell in enumerate(line):
+                paper_count = 0
+                print(f"Matrix Position: {cell} [{i},{j}]")
 
-            if paper_matrix[i, j] == '@':
-                paper_matrix[i, j] = 'X'
-                l_start = max(i - 1, 0)
-                l_end = min(i + 2, 140)
-                c_start = max(j - 1, 0)
-                c_end = min(j + 2, 140)
-                neighborhood = paper_matrix[l_start:l_end, c_start:c_end]
-                paper_count = neighborhood.flatten()
-                paper_count = paper_count == '@'
-                paper_count = np.sum(paper_count)
-                print(f"Line:{l_start}, {l_end} Column: {c_start}, {c_end} Count = {paper_count}")
-                print(neighborhood)
-                paper_matrix[i, j] = '@'
-                if paper_count < 4:
-                    available_paper_rolls += 1
-    print(available_paper_rolls)
+                if paper_matrix[i, j] == '@':
+                    paper_matrix[i, j] = 'X'
+                    l_start = max(i - 1, 0)
+                    l_end = min(i + 2, 140)
+                    c_start = max(j - 1, 0)
+                    c_end = min(j + 2, 140)
+                    neighborhood = paper_matrix[l_start:l_end, c_start:c_end]
+                    paper_count = neighborhood.flatten()
+                    paper_count = paper_count == '@'
+                    paper_count = np.sum(paper_count)
+                    print(f"Line:{l_start}, {l_end} Column: {c_start}, {c_end} Count = {paper_count}")
+                    print(neighborhood)
+                    #paper_matrix[i, j] = '@'
+                    if paper_count < 4:
+                        available_paper_rolls += 1
+                        removable_rolls = True
+                    else:
+                        paper_matrix[i, j] = '@'
+        print(available_paper_rolls)
     
 
     
